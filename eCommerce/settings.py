1890c5faeccbfbae6d5546671cb99742fd5d16e7
Dhairya <56044321+Dhairya2811@users.noreply.github.com>
2022-09-20 12:52:01 -0800

    Update settings.py

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/"static"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mail/static'),)
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
