8711863f1453ff48f8e31f12c083b982542180ab
dhairya2711 <dsdrago27@gmail.com>
2022-09-20 17:01:11 -0800

    some changes for heroku

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/"static"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mail/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'eCommerce_app/static'),)
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
